96438ad16ef183b86c264dbc605b9b1452740e53
Luis Manuel Diaz <luis.diaz@quind.io>
2024-08-29 00:12:00 -0500

    changes in conditionals

--- src/main/java/com/example/PruebaTecnicaJAVAQuind/aplication/service/PropertyService.java
+++ src/main/java/com/example/PruebaTecnicaJAVAQuind/aplication/service/PropertyService.java
@@ -37,6 +37,36 @@ public class PropertyService implements PropertyUseCase {
 
     @Override
     public Property createProperty(Property property) {
+        if(property.getName() == null ||
+                property.getDeleted() == null ||
+                property.getAvailable() == null ||
+                property.getCreationDate() == null ||
+                property.getPrice() == 0 ||
+                property.getAssociatedImageURL() == null ||
+                property.getUbication() == null){
+            throw new CustomException(HttpStatus.FORBIDDEN, "Ningun campo tiene que estar nulo");
+        }
+        if(property.getPrice() <= 0){
+            throw new CustomException(HttpStatus.FORBIDDEN, "El precio no puede ser negativo");
+        }
+        if(propertyUseCase.getProperty(property.getName()).isPresent()){
+            throw new CustomException(HttpStatus.FORBIDDEN, "Ya existe una propiedad con el nombre indicado");
+        }
+        if(!(property.getUbication().toLowerCase().contains("medellín") ||
+                property.getUbication().toLowerCase().contains("medellin") ||
+                property.getUbication().toLowerCase().contains("bogota") ||
+                property.getUbication().toLowerCase().contains("bogotá") ||
+                property.getUbication().toLowerCase().contains("cali") ||
+                property.getUbication().toLowerCase().contains("cartagena") )) {
+            throw new CustomException(HttpStatus.FORBIDDEN, "la propiedad tiene que estar ubicada en Medellín, Bogotá, Cali o Cartagena");
+        }
+        if((property.getUbication().toLowerCase().contains("bogota") ||
+                property.getUbication().toLowerCase().contains("bogotá") ||
+                property.getUbication().toLowerCase().contains("cali")) && property.getPrice()<2000000){
+            throw new CustomException(HttpStatus.FORBIDDEN, "Si la propiedad se encuentra en Bogotá o Cali esta debe tener un costo de alquiler " +
+                    "de más de 2 millones de pesos");
+        }
+
         return propertyUseCase.createProperty(property);
     }
 
@@ -52,14 +82,39 @@ public class PropertyService implements PropertyUseCase {
 
     @Override
     public Optional<Property> updateProperty(Property updateProperty) {
-        return Optional.ofNullable(this.createProperty(updateProperty/*, true*/));
+        Property currentProperty = this.getProperty(updateProperty.getName()).orElse(null);
+
+        if(currentProperty == null){
+            throw new CustomException(HttpStatus.NOT_FOUND, "Propiedad no encontrada");
+        }
+
+        if(!currentProperty.getAvailable()){
+            if(currentProperty.getPrice() != updateProperty.getPrice()){
+                throw new CustomException(HttpStatus.FORBIDDEN, "Si la propiedad está arrendada no se podrá cambiar su precio");
+            }
+            if(currentProperty.getUbication() != updateProperty.getUbication()){
+                throw new CustomException(HttpStatus.FORBIDDEN, "Si la propiedad está arrendada no se podrá cambiar su ubicación");
+            }
+        }
+
+        return Optional.ofNullable(this.createProperty(updateProperty));
     }
 
     public Optional<Property> deleteProperty(String name) {
         Property property = propertyUseCase.getProperty(name).orElse(null);
 
         if(property != null){
-            property.setDeleted(true);
+            LocalDateTime creationDate = property.getCreationDate();
+            LocalDateTime now = LocalDateTime.now();
+
+            LocalDateTime oneMonthLater = creationDate.plusMonths(1);
+
+            if(now.isBefore(oneMonthLater)){
+                property.setDeleted(true);
+            } else{
+                throw new CustomException(HttpStatus.FORBIDDEN, "Para que una propiedad pueda ser eliminada estar debe haber" +
+                        " sido guardada hace más de 1 mes");
+            }
         }
 
         return Optional.of(property);
@@ -74,9 +129,9 @@ public class PropertyService implements PropertyUseCase {
         Property leasedProperty = this.getProperty(leasePropertyInput.getPropertyName()).orElse(null);
 
         if(leasedProperty != null){
-            clientService.leasePropertyToClient(leasePropertyInput);
-            leasedProperty.setAvailable(false);
-            propertyUseCase.updateProperty(leasedProperty);
+            if(!leasedProperty.getAvailable() || leasedProperty.getDeleted()){
+                throw new CustomException(HttpStatus.NOT_FOUND, "Esta propiedad no se encuentra disponible");
+            }
         } else{
             throw new CustomException(HttpStatus.NOT_FOUND, "No hay ninguna propiedad con el nombre indicado");
         }
